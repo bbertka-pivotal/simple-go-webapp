72d0c13f1ac3e578727221d9b3284822661bd98f
bbertka-pivotal <bbertka@pivotal.io>
2018-02-15 15:24:44 -0800

    Updated logs spacing

--- app.go
+++ app.go
@@ -20,7 +20,7 @@ func main() {
 
 func Log(handler http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+        log.Printf("%s  %s  %s", r.RemoteAddr, r.Method, r.URL)
 	      handler.ServeHTTP(w, r)
     })
 }
